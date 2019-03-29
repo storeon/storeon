dc475e3d230afaa28cb965f3a242f2bcceb311db
Andrey Sitnik <andrey@sitnik.ru>
2019-03-29 13:29:26 -0400

    Add useMemo

--- preact/hook.js
+++ preact/hook.js
@@ -6,22 +6,24 @@ module.exports = function () {
   var keys = [].slice.call(arguments)
 
   var store = hooks.useContext(StoreContext)
-  var rerender = hooks.useState()[1]
-  var state = store.get()
+  var rerender = hooks.useState({ })
 
   hooks.useEffect(function () {
     return store.on('@changed', function (_, changed) {
       var changesInKeys = keys.some(function (key) {
         return key in changed
       })
-      if (changesInKeys) rerender({ })
+      if (changesInKeys) rerender[1]({ })
     })
   }, [])
 
-  var data = { }
-  keys.forEach(function (key) {
-    data[key] = state[key]
-  })
-  data.dispatch = store.dispatch
-  return data
+  return hooks.useMemo(function () {
+    var state = store.get()
+    var data = { }
+    keys.forEach(function (key) {
+      data[key] = state[key]
+    })
+    data.dispatch = store.dispatch
+    return data
+  }, [rerender[0]])
 }
--- react/hook.js
+++ react/hook.js
@@ -25,22 +25,24 @@ module.exports = function () {
   var keys = [].slice.call(arguments)
 
   var store = React.useContext(StoreContext)
-  var rerender = React.useState()[1]
-  var state = store.get()
+  var rerender = React.useState({ })
 
   React.useEffect(function () {
     return store.on('@changed', function (_, changed) {
       var changesInKeys = keys.some(function (key) {
         return key in changed
       })
-      if (changesInKeys) rerender({ })
+      if (changesInKeys) rerender[1]({ })
     })
   }, [])
 
-  var data = { }
-  keys.forEach(function (key) {
-    data[key] = state[key]
-  })
-  data.dispatch = store.dispatch
-  return data
+  return React.useMemo(function () {
+    var state = store.get()
+    var data = { }
+    keys.forEach(function (key) {
+      data[key] = state[key]
+    })
+    data.dispatch = store.dispatch
+    return data
+  }, [rerender[0]])
 }
