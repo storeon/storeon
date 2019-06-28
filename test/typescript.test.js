e9d9b71867a79e328d2e4a956671a35fbb84eeee
majo44 <majo44@gmail.com>
2019-06-28 21:10:38 +0200

    #68

--- test/typescript.test.js
+++ test/typescript.test.js
@@ -1,15 +1,16 @@
-const ts = require('typescript');
-const path = require('path');
+var ts = require('typescript')
+var path = require('path')
 
 it('Should fail on unsuitable declaration of events types', function () {
-  let program = ts.createProgram([path.join(__dirname, 'typescript', 'typedEventsErrors.ts')], {
+  var program = ts.createProgram(
+      [path.join(__dirname, 'typescript', 'typedEventsErrors.ts')], {
     strict: true,
     noImplicitAny: true,
     allowSyntheticDefaultImports: true,
     noEmit: true,
     maxNodeModuleJsDepth: 1
-  });
-  let allDiagnostics = ts.getPreEmitDiagnostics(program);
+  })
+
   // provided example contains 6 errors
-  expect(allDiagnostics.length).toEqual(6);
-});
+  expect(ts.getPreEmitDiagnostics(program)).toHaveLength()(6)
+})
