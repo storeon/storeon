b775118b07f2eef5a862480e20948263f83c1837
majo44 <majo44@gmail.com>
2019-06-28 21:21:16 +0200

    Fix Lint

--- test/typescript.test.js
+++ test/typescript.test.js
@@ -3,13 +3,13 @@ var path = require('path')
 
 it('Should fail on unsuitable declaration of events types', function () {
   var program = ts.createProgram(
-      [path.join(__dirname, 'typescript', 'typedEventsErrors.ts')], {
-    strict: true,
-    noImplicitAny: true,
-    allowSyntheticDefaultImports: true,
-    noEmit: true,
-    maxNodeModuleJsDepth: 1
-  })
+    [path.join(__dirname, 'typescript', 'typedEventsErrors.ts')], {
+      strict: true,
+      noImplicitAny: true,
+      allowSyntheticDefaultImports: true,
+      noEmit: true,
+      maxNodeModuleJsDepth: 1
+    })
 
   // provided example contains 6 errors
   expect(ts.getPreEmitDiagnostics(program)).toHaveLength(6)
