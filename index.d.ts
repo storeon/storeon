63b2ba5e622a27689313dc82076157580b443775
majo44 <majo44@gmail.com>
2019-07-02 01:05:03 +0200

    #68  Full type safe TypeScript declaration. (#69)
    * #68
    
    * #68
    
    * Fix the test.
    
    * Fix Lint
    
    * Fix the test.
    
    * Apply jest test also for successful typescript compilation. Removing tsconfig.json. Removing typecheck scripts task.

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,26 @@
 declare namespace createStore {
-  export type Dispatch = (event: PropertyKey, data?: unknown) => void;
+  export type Dispatch<EventsDataTypesMap = any> = <Event extends keyof EventsDataTypesMap>(
+      event: Event, ...data: (EventsDataTypesMap[Event] extends (never | undefined) ? [never?] : [EventsDataTypesMap[Event]])) => void;
 
-  export interface Store<State = unknown> {
-    readonly on: (
-      event: PropertyKey,
-      handler: (state: Readonly<State>, data: any) => Partial<State> | Promise<void> | null | void
+  export interface Store<State = unknown, EventsDataTypesMap extends StoreonEvents<State> = any> {
+    readonly on: <Event extends keyof EventsDataTypesMap>(
+      event: Event,
+      handler: (state: Readonly<State>, data: EventsDataTypesMap[Event]) => Partial<State> | Promise<void> | null | void
     ) => () => void;
-    readonly dispatch: Dispatch;
+    readonly dispatch: Dispatch<EventsDataTypesMap>;
     readonly get: () => State;
   }
 
-  export type Module<State> = (store: Store<State>) => void;
+  export type Module<State, EventsDataTypesMap extends StoreonEvents<State> = any> = (store: Store<State, EventsDataTypesMap>) => void;
+
+  export interface StoreonEvents<State> {
+    '@init': never;
+    '@changed': Partial<State>;
+  }
 }
 
-declare function createStore<State>(
-  modules: Array<createStore.Module<State> | false>
-): createStore.Store<State>;
+declare function createStore<State, EventsDataTypesMap extends createStore.StoreonEvents<State> = any>(
+  modules: Array<createStore.Module<State, EventsDataTypesMap> | false>
+): createStore.Store<State, EventsDataTypesMap>;
 
 export = createStore;
