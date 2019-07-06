698902bd76572d052b19aced779174435a66c586
Dmitry Skrylnikov <dskrylnikov@vigo.ru>
2019-07-06 18:06:54 +0300

    Add events declaration in useStoreon

--- test/typescript/react.tsx
+++ test/typescript/react.tsx
@@ -1,6 +1,6 @@
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
-import createStore, {Store} from '../..'
+import createStore, {Store, StoreonEvents} from '../..'
 import useStoreon from '../../react'
 import connect from '../../react/connect'
 import StoreContext from '../../react/context'
@@ -9,15 +9,19 @@ interface State {
   a: number
 }
 
+interface EventsDataTypesMap extends StoreonEvents<State> {
+  'inc': undefined;
+}
+
 function init(store: Store<State>) {
   store.on('@init', () => ({ a: 0 }))
   store.on('inc', (state, data: number) => ({a: state.a + data}))
 }
 
-const store = createStore([init])
+const store = createStore<State, EventsDataTypesMap>([init])
 
 function Button() {
-  const {dispatch, a} = useStoreon<State>('a')
+  const {dispatch, a} = useStoreon<State, EventsDataTypesMap>('a')
 
   const onClick = React.useCallback(() => dispatch('inc'), [])
 
