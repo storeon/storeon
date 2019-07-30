3ccb32f194409212577289bad6439b3dfb8dc1a6
Hovhannes Babayan <hovhannesbabayan@workfront.com>
2019-07-30 17:48:57 +0400

    fix: pass options to Redux DevTools

--- devtools/index.d.ts
+++ devtools/index.d.ts
@@ -2,7 +2,7 @@ import {Store} from '..';
 
 declare const devtools: {
   <State>(store: Store<State>): void;
-  (): <State>(store: Store<State>) => void;
+  (options?: object): <State>(store: Store<State>) => void;
 };
 
 export = devtools;
